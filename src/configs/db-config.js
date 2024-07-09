330ab020307fb59c913f9aa18ed30df7eec8de29
thiagolewin <thiagolewin06@gmail.com>
2024-07-09 18:46:30 -0300

    a

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -1,6 +1,6 @@
 const config = {
-    user        : "sqlserver", // Database username
-    password    : "\biX0?N3[vXGHD5D", // Database password
+    user        : "DressIt", // Database username
+    password    : "asdjwi298hbn", // Database password
     server      : "10.1.0.7", // Server IP address
     database    : "DressIt", 
     port        : 1433,

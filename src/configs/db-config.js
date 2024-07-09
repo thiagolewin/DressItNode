dfe5b82c747b0f75f924ac75fa59ffc675c39e60
thiagolewin <thiagolewin06@gmail.com>
2024-07-09 17:11:00 -0300

    a

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -1,7 +1,7 @@
 const config = {
     user        : "DressIt", // Database username
     password    : "%FjAA=_~>[9Qd]`p", // Database password
-    server      : "34.66.198.143", // Server IP address
+    server      : "10.1.0.3", // Server IP address
     database    : "DressIt", 
     port        : 1433,
     options: {

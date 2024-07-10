2ca737319776ed9a1b656a0ebadf2a30f8831447
thiagolewin <thiagolewin06@gmail.com>
2024-07-10 17:49:09 -0300

    a

--- src/controllers/image-controller.js
+++ src/controllers/image-controller.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
-import ImageService from './../services/image-service.js';
+import ImageService from '../services/image-service.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,23 +8,41 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
+
+// Configuración de almacenamiento en disco con Multer
 const diskStorage = multer.diskStorage({
     destination: path.join(__dirname, '../../public/images'),
     filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
     }
 });
+
+// Middleware para validar el tipo y tamaño del archivo
 const validateImageMiddleware = (req, res, next) => {
+    // Verificar si 'background_url' está presente en req.files
+    if (!req.files || !req.files['background_url']) {
+        return res.status(400).json({ error: 'No se ha adjuntado ningún archivo de fondo' });
+    }
+
     const backgroundFile = req.files['background_url'][0];
-    const allowedMimes = ['image/jpeg', 'image/png', 'image/gif']; // Definir tipos MIME permitidos para imágenes
-    
+    const allowedMimes = ['image/jpeg', 'image/png', 'image/gif'];
+    const maxFileSize = 20 * 1024 * 1024; // 20 MB en bytes
+
+    // Validar tipo MIME
     if (!allowedMimes.includes(backgroundFile.mimetype)) {
         return res.status(400).json({ error: 'El archivo de fondo no es una imagen válida' });
     }
-    console.log("PASO")
+
+    // Validar tamaño del archivo
+    if (backgroundFile.size > maxFileSize) {
+        return res.status(400).json({ error: 'El tamaño del archivo de fondo excede el límite permitido de 20 MB' });
+    }
+
     // Si pasa la validación, continuar con el siguiente middleware
     next();
 };
+
+// Middleware de Multer para subir archivos
 const fileUpload = multer({
     storage: diskStorage
 }).fields([
@@ -34,13 +52,15 @@ const fileUpload = multer({
 
 const svc = new ImageService();
 
-router.post("/post", validateImageMiddleware,fileUpload, async  (req, res) => {
+// Ruta POST para manejar la carga de imágenes
+router.post("/post", validateImageMiddleware, fileUpload, async (req, res) => {
     try {
         const garmentUrl = req.body.garment_url;
         const backgroundFile = req.files['background_url'][0];
         const backgroundFileName = backgroundFile.filename;
-        console.log(backgroundFileName)
-        const response = await fetch(`http://34.16.216.43:8000/?background_url=https://dressitnode-uq2eh73iia-uc.a.run.app/images/${backgroundFileName}&garment_url=${garmentUrl}`)
+
+        // Lógica para procesar la imagen, por ejemplo, enviarla a otro servicio
+        const response = await fetch(`http://34.16.216.43:8000/?background_url=https://example.com/images/${backgroundFileName}&garment_url=${garmentUrl}`);
 
         if (response.ok) {
             const contentType = response.headers.get('content-type');
@@ -51,7 +71,6 @@ router.post("/post", validateImageMiddleware,fileUpload, async  (req, res) => {
             throw new Error(`Fetch failed with status ${response.status}`);
         }
     } catch (error) {
-        // Capturar cualquier error y devolver un mensaje de error al cliente
         console.error('Error in fetch:', error.message);
         res.status(500).json({ error: 'Internal server error' });
     }

b6c37c521a0e3b168f78e5e335b9ba0c985beb49
thiagolewin <thiagolewin06@gmail.com>
2024-07-09 21:00:42 -0300

    a

--- src/controllers/image-controller.js
+++ src/controllers/image-controller.js
@@ -1,23 +1,34 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import multer from 'multer';
 import ImageService from './../services/image-service.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 const diskStorage = multer.diskStorage({
     destination: path.join(__dirname, '../images'),
-    filename: (req,file,cb)=> {
-        cb(null,Date.now() + "-" + file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
     }
-})
+});
+
 const fileUpload = multer({
     storage: diskStorage
-}).single('image')
+}).fields([
+    { name: 'background_url', maxCount: 1 },
+    { name: 'garment_url', maxCount: 1 }
+]);
+
 const svc = new ImageService();
-router.post("/post",fileUpload,(req,res)=> {
-    console.log(req.file)
-})
-export default router
+
+router.post("/post", fileUpload, (req, res) => {
+    const garmentUrl = req.body.garment_url;
+    const savedFilePath = req.files['background_url'][0].path
+    let resultado = fetch(`http://http://34.16.216.43:8000/?background_url=${savedFilePath}&garment_url=${garmentUrl}`)
+    res.status(200).json({ message: 'Upload successful', files: req.files, garmentUrl });
+});
+
+export default router;
